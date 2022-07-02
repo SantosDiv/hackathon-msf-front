870bcfb1b7c3bee7b121814c8d4f08def6b62437
lucasccosta <lucas_ccg@hotmail.com>
2022-07-02 16:10:00 -0300

    criação da pagina de autor

--- src/views/components/AuthorsTable.vue
+++ src/views/components/AuthorsTable.vue
@@ -1,7 +1,16 @@
+<style scoped>
+  .column {
+    padding: .75rem 1.5rem !important;
+  }
+  .space {
+    margin-right: .50em !important;
+  }
+</style>
+
 <template>
   <div class="card mb-4">
     <div class="card-header pb-0">
-      <h6>Authors table</h6>
+      <h6>Atores</h6>
     </div>
     <div class="card-body px-0 pt-0 pb-2">
       <div class="table-responsive p-0">
@@ -9,291 +18,67 @@
           <thead>
             <tr>
               <th
-                class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7"
+                class="text-uppercase text-secondary text-xs font-weight-bolder opacity-7"
               >
-                Author
+                Nome do Ator
               </th>
               <th
-                class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2"
+                class="text-uppercase text-secondary text-xs font-weight-bolder opacity-7 ps-2"
               >
-                Function
+                Cargo
               </th>
               <th
-                class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7"
+                class="text-center text-uppercase text-secondary text-xs font-weight-bolder opacity-7"
               >
-                Status
+                Redes Sociais
               </th>
               <th
-                class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7"
+                class="text-center text-uppercase text-secondary text-xs font-weight-bolder opacity-7"
               >
-                Employed
+                Apoiador
               </th>
-              <th class="text-secondary opacity-7"></th>
+              <th
+                class="text-center text-uppercase text-secondary text-xs font-weight-bolder opacity-7"
+              >
+                Gestão de Atores
+              </th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>
-                <div class="d-flex px-2 py-1">
-                  <div>
-                    <soft-avatar
-                      :img="img1"
-                      size="sm"
-                      border-radius="lg"
-                      class="me-3"
-                      alt="user1"
-                    />
-                  </div>
-                  <div class="d-flex flex-column justify-content-center">
-                    <h6 class="mb-0 text-sm">John Michael</h6>
-                    <p class="text-xs text-secondary mb-0">
-                      john@creative-tim.com
-                    </p>
-                  </div>
-                </div>
+            <tr v-for="stakeholder in stakeholders" :key="stakeholder.id">
+              <td class="column align-middle">
+                <p class="text-sm m-0">{{stakeholder.name}}</p>
               </td>
               <td>
-                <p class="text-xs font-weight-bold mb-0">Manager</p>
-                <p class="text-xs text-secondary mb-0">Organization</p>
+                <p class="text-sm font-weight-bold mb-0">{{stakeholder.position}}</p>
+                <p class="text-sm text-secondary mb-0">{{stakeholder.organization}}</p>
               </td>
-              <td class="align-middle text-center text-sm">
-                <soft-badge color="success" variant="gradient" size="sm"
-                  >Online</soft-badge
+              <td class="column align-middle text-center text-sm">
+                <a
+                  href="https://www.instagram.com/{{stakeholder.instagram_handler}}"
                 >
+                  <i class="fa fa-brands fa-instagram space fs-2"></i>
+                </a>
+                <a
+                  href="https://twitter.com/{{stakeholder.twitter_handler}}"
+                >
+                <i class="fa fa-brands fa-twitter" aria-hidden="true"></i>
+                </a>
               </td>
               <td class="align-middle text-center">
-                <span class="text-secondary text-xs font-weight-bold"
-                  >23/04/18</span
-                >
-              </td>
-              <td class="align-middle">
-                <a
-                  href="javascript:;"
-                  class="text-secondary font-weight-bold text-xs"
-                  data-toggle="tooltip"
-                  data-original-title="Edit user"
-                  >Edit</a
-                >
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <div class="d-flex px-2 py-1">
-                  <div>
-                    <soft-avatar
-                      :img="img2"
-                      class="me-3"
-                      size="sm"
-                      border-radius="lg"
-                      alt="user2"
-                    />
-                  </div>
-                  <div class="d-flex flex-column justify-content-center">
-                    <h6 class="mb-0 text-sm">Alexa Liras</h6>
-                    <p class="text-xs text-secondary mb-0">
-                      alexa@creative-tim.com
-                    </p>
-                  </div>
-                </div>
-              </td>
-              <td>
-                <p class="text-xs font-weight-bold mb-0">Programator</p>
-                <p class="text-xs text-secondary mb-0">Developer</p>
-              </td>
-              <td class="align-middle text-center text-sm">
-                <soft-badge color="secondary" variant="gradient" size="sm"
-                  >Offline</soft-badge
-                >
+                <span class="text-secondary text-sm font-weight-bold">
+                  {{stakeholder.role}}
+                </span>
               </td>
               <td class="align-middle text-center">
-                <span class="text-secondary text-xs font-weight-bold"
-                  >11/01/19</span
-                >
-              </td>
-              <td class="align-middle">
                 <a
-                  href="javascript:;"
-                  class="text-secondary font-weight-bold text-xs"
-                  data-toggle="tooltip"
-                  data-original-title="Edit user"
-                  >Edit</a
+                  href="https://localhost:3000/stakeholder/{{stakeholder.id}}/edit"
                 >
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <div class="d-flex px-2 py-1">
-                  <div>
-                    <soft-avatar
-                      :img="img3"
-                      size="sm"
-                      border-radius="lg"
-                      class="me-3"
-                      alt="user3"
-                    />
-                  </div>
-                  <div class="d-flex flex-column justify-content-center">
-                    <h6 class="mb-0 text-sm">Laurent Perrier</h6>
-                    <p class="text-xs text-secondary mb-0">
-                      laurent@creative-tim.com
-                    </p>
-                  </div>
-                </div>
-              </td>
-              <td>
-                <p class="text-xs font-weight-bold mb-0">Executive</p>
-                <p class="text-xs text-secondary mb-0">Projects</p>
-              </td>
-              <td class="align-middle text-center text-sm">
-                <soft-badge color="success" variant="gradient" size="sm"
-                  >Online</soft-badge
-                >
-              </td>
-              <td class="align-middle text-center">
-                <span class="text-secondary text-xs font-weight-bold"
-                  >19/09/17</span
-                >
-              </td>
-              <td class="align-middle">
-                <a
-                  href="javascript:;"
-                  class="text-secondary font-weight-bold text-xs"
-                  data-toggle="tooltip"
-                  data-original-title="Edit user"
-                  >Edit</a
-                >
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <div class="d-flex px-2 py-1">
-                  <div>
-                    <soft-avatar
-                      :img="img4"
-                      class="me-3"
-                      size="sm"
-                      border-radius="lg"
-                      alt="user4"
-                    />
-                  </div>
-                  <div class="d-flex flex-column justify-content-center">
-                    <h6 class="mb-0 text-sm">Michael Levi</h6>
-                    <p class="text-xs text-secondary mb-0">
-                      michael@creative-tim.com
-                    </p>
-                  </div>
-                </div>
-              </td>
-              <td>
-                <p class="text-xs font-weight-bold mb-0">Programator</p>
-                <p class="text-xs text-secondary mb-0">Developer</p>
-              </td>
-              <td class="align-middle text-center text-sm">
-                <soft-badge color="success" variant="gradient" size="sm"
-                  >Online</soft-badge
-                >
-              </td>
-              <td class="align-middle text-center">
-                <span class="text-secondary text-xs font-weight-bold"
-                  >24/12/08</span
-                >
-              </td>
-              <td class="align-middle">
-                <a
-                  href="javascript:;"
-                  class="text-secondary font-weight-bold text-xs"
-                  data-toggle="tooltip"
-                  data-original-title="Edit user"
-                  >Edit</a
-                >
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <div class="d-flex px-2 py-1">
-                  <div>
-                    <soft-avatar
-                      :img="img5"
-                      class="me-3"
-                      size="sm"
-                      border-radius="lg"
-                      alt="user5"
-                    />
-                  </div>
-                  <div class="d-flex flex-column justify-content-center">
-                    <h6 class="mb-0 text-sm">Richard Gran</h6>
-                    <p class="text-xs text-secondary mb-0">
-                      richard@creative-tim.com
-                    </p>
-                  </div>
-                </div>
-              </td>
-              <td>
-                <p class="text-xs font-weight-bold mb-0">Manager</p>
-                <p class="text-xs text-secondary mb-0">Executive</p>
-              </td>
-              <td class="align-middle text-center text-sm">
-                <soft-badge color="secondary" variant="gradient" size="sm"
-                  >Offline</soft-badge
-                >
-              </td>
-              <td class="align-middle text-center">
-                <span class="text-secondary text-xs font-weight-bold"
-                  >04/10/21</span
-                >
-              </td>
-              <td class="align-middle">
-                <a
-                  href="javascript:;"
-                  class="text-secondary font-weight-bold text-xs"
-                  data-toggle="tooltip"
-                  data-original-title="Edit user"
-                  >Edit</a
-                >
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <div class="d-flex px-2 py-1">
-                  <div>
-                    <soft-avatar
-                      :img="img6"
-                      class="me-3"
-                      size="sm"
-                      border-radius="lg"
-                      alt="user6"
-                    />
-                  </div>
-                  <div class="d-flex flex-column justify-content-center">
-                    <h6 class="mb-0 text-sm">Miriam Eric</h6>
-                    <p class="text-xs text-secondary mb-0">
-                      miriam@creative-tim.com
-                    </p>
-                  </div>
-                </div>
-              </td>
-              <td>
-                <p class="text-xs font-weight-bold mb-0">Programtor</p>
-                <p class="text-xs text-secondary mb-0">Developer</p>
-              </td>
-              <td class="align-middle text-center text-sm">
-                <soft-badge color="secondary" variant="gradient" size="sm"
-                  >Offline</soft-badge
-                >
-              </td>
-              <td class="align-middle text-center">
-                <span class="text-secondary text-xs font-weight-bold"
-                  >14/09/20</span
-                >
-              </td>
-              <td class="align-middle">
-                <a
-                  href="javascript:;"
-                  class="text-secondary font-weight-bold text-xs"
-                  data-toggle="tooltip"
-                  data-original-title="Edit user"
-                  >Edit</a
-                >
+                  <i class="fa fa-solid fa-pen-to-square" aria-hidden="true"></i>
+                </a>
+                <a>
+                  <i class="fa fa-solid fa-trash" aria-hidden="true" @click="deleteStakeholder(stakeholder.id)"></i>
+                </a>
               </td>
             </tr>
           </tbody>
@@ -304,30 +89,26 @@
 </template>
 
 <script>
-import SoftAvatar from "@/components/SoftAvatar.vue";
-import SoftBadge from "@/components/SoftBadge.vue";
-import img1 from "../../assets/img/team-2.jpg";
-import img2 from "../../assets/img/team-3.jpg";
-import img3 from "../../assets/img/team-4.jpg";
-import img4 from "../../assets/img/team-3.jpg";
-import img5 from "../../assets/img/team-2.jpg";
-import img6 from "../../assets/img/team-4.jpg";
+import Axios from '../../axios/index'
 
 export default {
   name: "authors-table",
   data() {
     return {
-      img1,
-      img2,
-      img3,
-      img4,
-      img5,
-      img6,
+      stakeholders: []
     };
   },
-  components: {
-    SoftAvatar,
-    SoftBadge,
+  methods: {
+    async setAllStakeholders(){
+      const response = await Axios.get('/stakeholder')
+      this.setAllStakeholders = response.data
+    },
+    async deleteStakeholder(id){
+      await Axios.delete(`{/stakeholder/${id}`)
+    }
   },
+  created() {
+    this.setAllStakeholders()
+  }
 };
 </script>
